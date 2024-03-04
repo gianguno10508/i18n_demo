6ba8b42588420b6c8598d9be67cd6d4d4aa2b305
Giang Nv <gianguno10508@gmail.com>
2024-03-04 12:13:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+function WelcomeMessage() {
+  const { t } = useTranslation();
+
+  return <h1>{t('welcome')}</h1>;
+}
+
+function Greeting({ name }) {
+  const { t } = useTranslation();
+
+  return <p>{t('greeting', { name })}</p>;
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WelcomeMessage />
+      <Greeting name="John" />
     </div>
   );
 }
-
-export default App;
---
+++ src/i18n.js
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslation from './locales/en.json';
+import frTranslation from './locales/fr.json';
+import viTranslation from './locales/vi.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      fr: { translation: frTranslation },
+      vi: { translation: viTranslation }
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
